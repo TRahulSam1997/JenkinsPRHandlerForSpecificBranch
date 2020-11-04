2ba15eda6995698458e75fc44594d1b1b01ff6cb
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2020-11-04 16:24:30 +1100

    initial commit

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ const port = process.env.PORT || 8080;
 // Tell express to use body-parser's JSON parsing
 app.use(bodyParser.json());
 
+// Start express on the defined port
+app.listen(port, () => console.log(`Listening...`));
+
 app.get("/", (req, res) => {
   res.send(`"Above all, don't lie to yourself. The man who lies to himself and listens to his own lie comes to a point that he cannot distinguish the truth within him, or around him, and so loses all respect for himself and for others. And having no respect he ceases to love." ― Fyodor Dostoevsky, The Brothers Karamazov`);
 });
